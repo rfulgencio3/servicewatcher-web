bdbcfb7f6e82786f286287522dfbe716aa336960
rfulgencio3 <rfulgencio3@gmail.com>
2024-06-10 16:54:14 -0300

    Ajsute rota

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -11,7 +11,7 @@ const SignUp = () => {
   const handleSignUp = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://servicewatcher-authservice.azurewebsites.net/api/auth/registrer', {
+      const response = await fetch('https://servicewatcher-authservice.azurewebsites.net/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

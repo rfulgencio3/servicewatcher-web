667dc2af7132c77fc036b142eb4c70ec4eae8c7e
rfulgencio3 <rfulgencio3@gmail.com>
2024-06-11 17:36:39 -0300

    Atualização para visualização de senha e confirmPassword em SignUp

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,17 +1,39 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './SignUp.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const SignUp = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordMatch, setPasswordMatch] = useState(true);
+  const [showErrorModal, setShowErrorModal] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const navigate = useNavigate();
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    setPasswordMatch(e.target.value === confirmPassword);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+    setPasswordMatch(e.target.value === password);
+  };
 
   const handleSignUp = async (e) => {
     e.preventDefault();
+
+    if (!passwordMatch) {
+      setShowErrorModal(true);
+      return;
+    }
+
     try {
-      const response = await fetch('https://servicewatcher-authservice.azurewebsites.net/api/auth/register', {
+      const response = await fetch('https://servicewatcher-authservice.azurewebsites.net/api/Auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -20,15 +42,18 @@ const SignUp = () => {
       });
 
       if (response.ok) {
-        // Redirecionar para a tela de escolha de plano
         navigate('/choose-plan');
       } else {
-        alert('Failed to register. Please try again.');
+        setShowErrorModal(true);
       }
     } catch (error) {
       console.error('Error:', error);
-      alert('Failed to register. Please try again.');
+      setShowErrorModal(true);
     }
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
   };
 
   return (
@@ -37,7 +62,7 @@ const SignUp = () => {
       <form onSubmit={handleSignUp}>
         <label>Name</label>
         <input
-          type="name"
+          type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
           required
@@ -50,14 +75,41 @@ const SignUp = () => {
           required
         />
         <label>Password</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Sign Up</button>
+        <div className="password-container">
+          <input
+            type={passwordVisible ? "text" : "password"}
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+          <span className="password-toggle-icon" onClick={togglePasswordVisibility}>
+            <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} />
+          </span>
+        </div>
+        <label>Confirm Password</label>
+        <div className="password-container">
+          <input
+            type={passwordVisible ? "text" : "password"}
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            required
+          />
+          <span className="password-toggle-icon" onClick={togglePasswordVisibility}>
+            <FontAwesomeIcon icon={passwordVisible ? faEyeSlash : faEye} />
+          </span>
+        </div>
+        {!passwordMatch && <p className="error-message">Passwords do not match</p>}
+        <button type="submit" disabled={!passwordMatch}>Sign Up</button>
       </form>
+
+      {showErrorModal && (
+        <div className="error-modal">
+          <div className="error-modal-content">
+            <span className="close-button" onClick={() => setShowErrorModal(false)}>&times;</span>
+            <p>Failed to register. Please try again.</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
